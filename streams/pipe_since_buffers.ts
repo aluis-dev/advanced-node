48066e02787c121e6013e116d1db29038c74f34c
luisdev <luislazcano.dev@outlook.com>
2025-06-22 12:17:33 -0600

    Implement file copying using streams with buffer handling

---
+++ streams/pipe_since_buffers.ts
@@ -0,0 +1,21 @@
+import { open } from 'node:fs/promises';
+
+(async () => {
+    const srcFile = await open('src.txt', 'r');
+    const destFile = await open('copied.txt', 'w');
+
+    let bytesRead = -1;
+    while(bytesRead !== 0) {
+        const readResult = await srcFile.read();
+        bytesRead = readResult.bytesRead;
+
+        if(bytesRead !== 16384) {
+            const indexNotFilled = readResult.buffer.indexOf(0);
+            const buff = Buffer.alloc(indexNotFilled);
+            readResult.buffer.copy(buff, 0, 0, indexNotFilled);
+            destFile.write(buff);
+            continue;
+        }
+        destFile.write(readResult.buffer);
+    }
+})();
